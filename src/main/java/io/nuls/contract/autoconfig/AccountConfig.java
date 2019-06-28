2094caa6236d78e4693a3559b4a904bfb03aaf0b
meilongwhpu <meilong_whpu@163.com>
2019-06-28 17:31:54 +0800

    commit

--- src/main/java/io/nuls/contract/autoconfig/AccountConfig.java
+++ src/main/java/io/nuls/contract/autoconfig/AccountConfig.java
@@ -8,7 +8,6 @@ import io.nuls.core.rockdb.constant.DBErrorCode;
 import io.nuls.core.rockdb.service.RocksDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.io.File;
 
 @Component
