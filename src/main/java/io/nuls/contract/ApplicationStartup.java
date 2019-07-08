be4b506b639b3a1f2b98202ba8caf4c0aedf5124
MIMIEYES <pierreluohao@gmail.com>
2019-07-09 00:24:22 +0800

    local test

--- src/main/java/io/nuls/contract/ApplicationStartup.java
+++ src/main/java/io/nuls/contract/ApplicationStartup.java
@@ -1,9 +1,13 @@
 package io.nuls.contract;
 
+import com.googlecode.jsonrpc4j.spring.JsonServiceExporter;
 import io.nuls.contract.autoconfig.ApiModuleInitTool;
+import io.nuls.contract.model.TempErrorResolver;
 import io.nuls.core.log.Log;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+
+import java.util.Map;
 
 public class ApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -14,6 +18,11 @@ public class ApplicationStartup implements ApplicationListener<ContextRefreshedE
         try {
             Log.info("----------run Application Startup Listener-------------");
             dataInitTool.init();
+            Map<String, JsonServiceExporter> beansOfType = contextRefreshedEvent.getApplicationContext().getBeansOfType(JsonServiceExporter.class);
+            for(JsonServiceExporter bean : beansOfType.values()) {
+                bean.setErrorResolver(TempErrorResolver.INSTANCE);
+                bean.afterPropertiesSet();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
