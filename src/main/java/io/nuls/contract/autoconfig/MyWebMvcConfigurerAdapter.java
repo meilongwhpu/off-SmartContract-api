b76cac2d7c02fe0b6d6634e501ac6019d3fe7edf
meilongwhpu <meilong_whpu@163.com>
2019-07-11 17:35:38 +0800

    设置缺省值

--- src/main/java/io/nuls/contract/autoconfig/MyWebMvcConfigurerAdapter.java
+++ src/main/java/io/nuls/contract/autoconfig/MyWebMvcConfigurerAdapter.java
@@ -1,19 +1,9 @@
 package io.nuls.contract.autoconfig;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 /**
  * @author: PierreLuo
